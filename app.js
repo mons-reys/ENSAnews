b91b4ec1e7d6cc0347b780fab37d262c7ad79a13
mons-reys <mons.reys1@gmail.com>
2021-02-12 15:40:41 +0100

    added and styled the index page

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+//listen for a request 
+app.listen(3000);
+
+//middleware and static files
+app.use(express.static('public'));
+
+/* app.use((req, res, next) => {
+    console.log('try middlewares');
+    next();
+}) */
+
+//set the ejs view engine
+app.set('view engine', 'ejs');
+
+//responsre to a request 
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+app.get('/services', (req, res) =>{
+    res.render('services');
+});
+app.get('/about', (req, res) =>{
+    res.render('about');
+});
+app.get('/blogs/create', (req, res) =>{
+    res.render('create');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404');
+})
